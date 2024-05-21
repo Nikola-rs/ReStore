267ea6ab688f1e6f118f565e5bca67c1427b5b8d
Nikola <nikolaristov65@gmail.com>
2024-05-21 17:17:36 +0200

    End of section 6

---
+++ client/src/app/util/util.ts
@@ -0,0 +1,8 @@
+export function getCookie(key: string){
+    const c = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
+    return c ? c.pop() : "";
+}
+
+export function currencyFormat(amount: number){
+    return '$' + (amount/100).toFixed(2);
+}
